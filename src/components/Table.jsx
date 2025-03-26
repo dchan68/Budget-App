19afaa22cba659abd78a8e488d4b6b6c022d8572
Daryan Chan <daryan.j.chan@hotmail.com>
2025-03-26 13:49:10 -0400

    creating budget page

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import ExpenseItem from './ExpenseItem'
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
 
-function Table({expenses}) {
+function Table({ expenses, showBudget = true }) {
+  //defaulting showBudget to true if no showBudget parameter was received
   return (
-        <div className="table">
-            <table>
-                <thead>
-                <tr>
-                    {
-                    ["Name", "Amount", "Date", "Budget", ""].map((i, index) => (
-                        <th key={index}>{i}</th>
-                    ))
-                    }
-                </tr>
-                </thead>
-                <tbody>
-                {
-                    expenses.map((expense) => (
-                    <tr key={expense.id}>
-                        <ExpenseItem expense={expense} />
-                    </tr>
-                    ))
-                }
-                </tbody>
-            </table>
-            </div>
-  )
+    <div className="table">
+      <table>
+        <thead>
+          <tr>
+            {/* These are the table header column names */}
+            {["Name", "Amount", "Date", showBudget ? "Budget" : "", ""].map(
+              (i, index) => (
+                <th key={index}>{i}</th>
+              )
+            )}
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense) => (
+            <tr key={expense.id}>
+              <ExpenseItem expense={expense} showBudget={showBudget}/>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default Table
+export default Table;
